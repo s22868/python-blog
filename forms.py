cd55ff23104b8967aa5e6a0345fea80485c9828d
Mati <mati.leeder@gmail.com>
2023-06-17 01:58:12 +0200

    fix email field

--- forms.py
+++ forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.widgets import TextArea
+from wtforms.widgets import TextArea, EmailInput
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email(), Length(min=2)])
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(min=2)], widget=EmailInput())
     password = PasswordField("Hasło", validators=[DataRequired()])
     submit = SubmitField("Zaloguj się")
 
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email(), Length(min=2)])
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(min=2)], widget=EmailInput())
     password = PasswordField("Hasło", validators=[DataRequired()])
     password2 = PasswordField(
         "Powtórz hasło", validators=[DataRequired(), EqualTo("password")]
